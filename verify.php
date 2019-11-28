b90e0a3d700ae5e2ac62c6a448fd4745494b41d2
Peachvon <58198950+Peachvon@users.noreply.github.com>
2019-11-28 10:14:12 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'SfYk5HulxIOqkOWbnwQSB6HmpWT2vH4izdqR27jh/YYJWECxy6idOwSD54e5fgGQx3/iyY8x/biuundzYlJFLZxtwNPC3WNWLFRzNfwwPDRTcYST3dWH9QvhbbdmNIpzXASvpKjyye91gZJi8x49AwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
